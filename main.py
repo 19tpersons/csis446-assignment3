fb46ce08d8da76e5de14da3243982ae6816f4295
Noah-Vetrone <93504006+Noah-Vetrone@users.noreply.github.com>
2022-03-26 23:16:38 -0500

    Update main.py
    Code created to generate RNA and DNA sequences, generates it as a string, doesn't put it into a list. depending on how we want to do it we don't need to use lists.

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-from random import rand
+import random
 
-SEQUENCES = [] #A list of the generated sequence
+SEQUENCELIST = [] #A list of the generated sequence
 SEQUENCETYPE = "RNA" #RNA || DNA
 NUMBEROFSEQUENCES = 5 #The number of sequences to be generated
 SEQUENCELENGTH = 100 #The length of each sequence
-SUBSEQUENCE = "AGBT" #A subseqence of the RNA/DNA that the program is going to try to find.
+SUBSEQUENCE = "ACGT" #A subseqence of the RNA/DNA that the program is going to try to find.
 
 def CreateDNASequence(): #Create a random DNA sequence of length SEQUENCELENGTH
-  pass
+    SEQUENCE = ""
+    for i in range(0,SEQUENCELENGTH):
+        SEQUENCE += random.choice("ACGT")
+    return SEQUENCE
 
 def CreateRNASequence(): #Create a random RNA sequence of length SEQUENCELENGTH
-  pass
+    SEQUENCE = ""
+    for i in range(0,SEQUENCELENGTH):
+        SEQUENCE += random.choice("ACGU")
+    return SEQUENCE
 
 def WriteSequences(): #Generates and writes the sequences to a file. 
   if SEQUENCETYPE == 'RNA':
     for i in range(NUMBEROFSEQUENCES):
-      CreateRNASequence()
+      SEQUENCE = CreateRNASequence()
   elif SEQUENCETYPE == 'DNA':
     for i in range(NUMBEROFSEQUENCES):
-      CreateDNASequence()
+      SEQUENCE = CreateDNASequence()
 
   #Write SEQUENCES to a file.
   
